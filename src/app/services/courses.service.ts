b60ed4ae9adaf6ac494947d8db3e9262f2c17e06
Zinnia Rana <zinnia.rana.22@gmail.com>
2023-07-04 21:55:01 -0400

    View Course Reactive Style

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -11,6 +11,26 @@ import { Lesson } from "../model/lesson";
 export class CoursesService {
     constructor(private http: HttpClient) {}
 
+    loadCourseById(courseId: number): Observable<Course> {
+        return this.http.get<Course>(`/api/courses/${courseId}`)
+            .pipe(
+                shareReplay()
+            )
+    }
+
+    loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+        return this.http.get<Lesson[]>('/api/lessons', {
+            params: {
+                pageSize: "200",
+                courseId: courseId.toString()
+            }
+        })
+        .pipe(
+            map(res => res['payload']),
+            shareReplay()
+        )
+    }
+
     loadAllCourses(): Observable<Course[]> {
         return this.http.get<Course[]>('/api/courses')
             .pipe(
